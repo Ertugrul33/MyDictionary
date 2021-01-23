7f97b63c4cc91fc8207dfc7321b647825a98be73
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-01-23 17:18:29 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ogrenci1 = new Dictionary<int, string>();
+            ogrenci1.Add(101, "Engin");
+            Console.WriteLine(ogrenci1.Count);
+
+            MyDictionary<int, string> ogrenci2 = new MyDictionary<int, string>();
+            ogrenci2.Add(102, "Ertuğrul");
+            Console.WriteLine(ogrenci2.Count);
+        }
+    }
+
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _keyarray;
+        TKey[] _keytempArray;
+
+        TValue[] _valuearray;
+        TValue[] _valuetempArray;
+
+        public MyDictionary()
+        {
+            _keyarray = new TKey[0];
+            _valuearray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _keytempArray = _keyarray;
+            _keyarray = new TKey[_keyarray.Length + 1];
+
+            _valuetempArray = _valuearray;
+            _valuearray = new TValue[_valuearray.Length + 1];
+
+            for (int i = 0; i < _keytempArray.Length; i++)
+            {
+                _keyarray[i] = _keytempArray[i]; 
+            }
+            _keyarray[_keyarray.Length - 1] = key;
+
+            for (int i = 0; i < _valuetempArray.Length; i++)
+            {
+                _valuearray[i] = _valuetempArray[i];
+            }
+            _valuearray[_valuearray.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return _keyarray.Length; } //Sadece key'in kaç elemanlı olduğunu göstermemiz yeterli.
+        }
+    }
+}
